6c139c717602b8176014f3987e90f79444581c3b
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2025-02-27 21:47:01 +0000

    Add initial project structure with FastAPI setup and Azure infrastructure

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import sys
+
+app = FastAPI()
+
+@app.get("/")
+async def index():
+    version = sys.version_info
+    return PlainTextResponse(content=f"Hello World, I am Python {version.major}.{version.minor}")
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
